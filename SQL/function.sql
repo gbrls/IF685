d04c9125fe4e3c43d52ffa24f51087b27c9c3158
Vinicius Novaes <vcn2@cin.ufpe.br>
2021-08-26 06:50:56 -0300

    Corrções de insert agr sim carai

---
+++ SQL/function.sql
@@ -0,0 +1,20 @@
+CREATE FUNCTION greatestDoctor(nome_medicamento VARCHAR(255)) RETURN VARCHAR(50) AS 
+BEGIN 
+    codigo_medicamento NUMBER(6); 
+    SELECT codigo INTO codigo_medicamento FROM medicamento WHERE nome = nome_medicamento;
+    CURSOR medicos_cpf IS SELECT cpf FROM medico;
+    medico_cpf %rowtype; 
+    greatest_doctor medico%nome;
+    maior_uso INTEGER ;
+    maior_uso := 0;
+    atual_uso INTEGER;
+    atual_uso := 0;
+    FOR medico_cpf IN medicos_cpf LOOP
+        SELECT COUNT(codigo) INTO atual_uso FROM UTILIZA WHERE codigo = codigo_medicamento AND cpf_medico = medico_cpf;
+        IF atual_uso > maior_uso THEN
+            maior_uso := atual_uso
+            SELECT nome INTO greatest_doctor FROM medico WHERE cpf = medico_cpf;
+        END IF;
+    END LOOP;
+    RETURN greatest_doctor;
+END;
